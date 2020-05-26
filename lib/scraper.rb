4a8988a1d34976b736b84689db2b0f113f393822
claudia v <claudia4ro@yahoo.com>
2020-05-26 20:29:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,8 +27,30 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    attributes = {}
+
+    doc.css("div.social-icon-container a").each do |xml|
+      case xml.attribute("href").value
+
+      when /github/
+        attributes[:github] = xml.attribute("href").value
+      when /linkedin/
+        attributes[:linkedin] = xml.attribute("href").value      
+      when /twitter/
+        attributes[:twitter] = xml.attribute("href").value
+      else
+        attributes[:blog] = xml.attribute("href").value
+      end
+
+    end
+    attributes[:profile_quote] = doc.css("div.profile-quote").text
+    attributes[:bio] = doc.css("div.bio-content div.description-holder").text.strip
+    attributes
   end
+    
+  
 
 end
 
