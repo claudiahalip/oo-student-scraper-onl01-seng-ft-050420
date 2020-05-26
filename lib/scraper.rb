ccc3e74fb4c84f8f7d93de715c76821f21bccb97
claudia v <claudia4ro@yahoo.com>
2020-05-26 21:30:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,25 +6,19 @@ class Scraper
   
 
   def self.scrape_index_page(index_url)
-    
-    
-    
-    file = Nokogiri::HTML(open(index_url))
-    
-    info  = file.css(".student-cards")
-    
-    students_arr = []    #{:name=>"Mathieu Balez", :location=>"New York, NY", :profile_url=>"students/mathieu-balez.html"}
-    
-    info.map do |c|
-      students_arr << { :name => c.css("h4.student-name").text,
-        :location => c.css("p.student-location").text,
-        :profile_url => c.css("a").attribute("href").value
-      }
-    end 
-      
-    students_arr
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    student_cards = doc.css(".student-card")
+    students = []
 
+    student_cards.map do |card|
+      students << {
+        :name => card.css("h4.student-name").text,
+        :location => card.css("p.student-location").text,
+        :profile_url => card.css("a").attribute("href").value
+      }
+    end
+    students
   end
   
   
