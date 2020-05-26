30cb47504f45525d7b2b33412514b4635d3c65d6
claudia v <claudia4ro@yahoo.com>
2020-05-26 20:02:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,14 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open ( "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+    
+    
+    file = Nokogiri::HTML(html)
+    
+    arr_hashes = file.css(".student_card")
+    binding.pry 
+    
     
   end
 
