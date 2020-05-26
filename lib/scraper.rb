ff115fc75dcdcc0268f0f13bdfdfdb26195ecbe5
claudia v <claudia4ro@yahoo.com>
2020-05-26 20:24:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,10 @@ class Scraper
     students_arr = []    #{:name=>"Mathieu Balez", :location=>"New York, NY", :profile_url=>"students/mathieu-balez.html"}
     
     info.each do |s|
-      students_arr << { name =>
+      students_arr << { :name => card.css("h4.student-name").text,
+        :location => card.css("p.student-location").text,
+        :profile_url => card.css("a").attribute("href").value
+      }
     
     binding.pry 
     
