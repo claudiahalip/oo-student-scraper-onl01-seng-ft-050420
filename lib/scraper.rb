8751c495d690d68bed8f32a09f2f07abdd5c0b0f
claudia v <claudia4ro@yahoo.com>
2020-05-26 21:02:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Scraper
   
-  attr_accessor :
+  attr_accessor :students
 
   def self.scrape_index_page(index_url)
     url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
